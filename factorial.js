b2f20b758ee8986256e7b3d0138e1450e5caaa07
Johnson11 <kalijoe11@gmail.com>
2023-06-27 15:04:47 +0300

    tdd

--- factorial.js
+++ factorial.js
@@ -1,19 +1,25 @@
 
-// Kalibbala Johnson
-// Diana Ajambo
-
-
 function factorial(n) {
     let result = n;
   
     for (let i = n - 1; i >= 1; i--) {
       result *= i;
+    
     }
-  
     return result;
   }
   
+// Using recurssion to get factorial
 
+  // function factorial(n) {
+  //   if (n === 0 || n === 1) {
+  //     return 1;
+  //   } else {
+  //     return n * factorial(n - 1);
+  //   }
+  // }
   
+ 
+
  module.exports = factorial;
   
